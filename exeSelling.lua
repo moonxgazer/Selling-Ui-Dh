3c1e17148ffd689334a7c7c40cfdec500c71a24d
moonxgazer <parasasia1@gmail.com>
2026-04-19 22:55:43 +0200

    Update exeSelling.lua

--- exeSelling.lua
+++ exeSelling.lua
@@ -1,21 +1,9 @@
-getgenv().SCRIPT_KEY = "c4352185-57e7-4898-8c45-d905a21a56b2"
+getgenv().SCRIPT_KEY = "put key here"
 
-getgenv().alts = {
-    "moonxgazer",
-    "37QTL",
-    "machiejulius",
-    "haloboy647",
-    "zoomsunnyultra",
-    "spacedanielbryan870",
-    "halfnatalie04965",
-    "wall_jillsmith755",
-    "carry_uwise763",
-    "natural_wyates16",
-    "consider_jill02274",
-    "perform_hunterlee45",
-    "might_pking462",
-    "studentweberrodney83",
-    "aoizhdaiozpodazjk"
+getgenv().alts = { -- no limits
+    "alt1",
+    "alt2",
+    "alt3"
 }
 
-loadstring(game:HttpGet("https://radiumscripts.vercel.app/s/23c6161b-57d8-48d7-befa-4cd4106fedd2/selling_ui.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/moonxgazer/Selling-Ui-Dh/refs/heads/main/mainSelling.lua"))()
