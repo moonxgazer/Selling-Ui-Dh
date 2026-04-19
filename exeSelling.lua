9b6058f5dca1d53576220c1098b0be15612131a8
moonxgazer <parasasia1@gmail.com>
2026-04-19 19:01:54 +0200

    Add files via upload

---
+++ exeSelling.lua
@@ -0,0 +1,21 @@
+getgenv().SCRIPT_KEY = "c4352185-57e7-4898-8c45-d905a21a56b2"
+
+getgenv().alts = {
+    "moonxgazer",
+    "37QTL",
+    "machiejulius",
+    "haloboy647",
+    "zoomsunnyultra",
+    "spacedanielbryan870",
+    "halfnatalie04965",
+    "wall_jillsmith755",
+    "carry_uwise763",
+    "natural_wyates16",
+    "consider_jill02274",
+    "perform_hunterlee45",
+    "might_pking462",
+    "studentweberrodney83",
+    "aoizhdaiozpodazjk"
+}
+
+loadstring(game:HttpGet("https://radiumscripts.vercel.app/s/23c6161b-57d8-48d7-befa-4cd4106fedd2/selling_ui.lua"))()
